e4840550b5cc397f2584b72ad7db01590c45d746
Роман <r0m-1k@yandex.ru>
2017-04-20 21:51:22 +0300

    created UI, added algorithm for converting the image to a text file

---
+++ ImageToASCIIConverter/ImageToASCIIConverter/Converters.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Media.Imaging;
+
+namespace ImageToASCIIConverter
+{
+    public class Converters
+    {
+        private readonly BitmapSource _sourceImage;
+        private const int DataFormatSize = 4;
+        private const double CharsCount = 10000;
+
+        public Converters(BitmapSource image)
+        {
+            _sourceImage = image;
+        }
+
+        public string GetTextFile()
+        {
+            var textFile = "";
+
+            var grayscaleArray = ImageToGrayscale(_sourceImage);
+            var grayscaleArrayResized = ResizeImage(grayscaleArray);
+
+            var averageColor = grayscaleArrayResized.Cast<double>().Sum() / grayscaleArrayResized.Length;
+            var chars = new[] {'.', '*'};
+            var counter = 0;
+
+            foreach (var pixel in grayscaleArrayResized)
+            {
+                if (counter == grayscaleArrayResized.GetLength(1))
+                {
+                    textFile += "\n";
+                    counter = 0;
+                }
+                if (pixel < averageColor)
+                {
+                    textFile += chars[0];
+                }
+                else
+                {
+                    textFile += chars[1];
+                }
+                counter++;
+            }
+
+            return textFile;
+        }
+
+        private double[,] ImageToGrayscale(BitmapSource image)
+        {
+            var grayscaleImage = new double[image.PixelHeight, image.PixelWidth];
+            var stride = image.PixelWidth * DataFormatSize;
+            var size = image.PixelHeight * stride;
+            var data = new byte[size];
+
+            image.CopyPixels(data, stride, 0);
+
+            for (int x = 0; x < image.PixelWidth; x++)
+            {
+                for (int y = 0; y < image.PixelHeight; y++)
+                {
+                    var pixelIndex = y * stride + DataFormatSize * x;
+
+                    grayscaleImage[y, x] = 0.299 * data[pixelIndex + 1] 
+                        + 0.5870 * data[pixelIndex + 2] 
+                        + 0.1140 * data[pixelIndex + 3];
+                }
+            }
+
+            return grayscaleImage;
+        }
+
+        private double[,] ResizeImage(double[,] image)
+        {
+            var ratio = image.Length / CharsCount;
+            var height = image.GetLength(1) / Math.Sqrt(ratio);
+            var width = image.GetLength(0) / Math.Sqrt(ratio);
+            var resizedImage = new double[(int)width, (int)height];
+            var ratioX = image.GetLength(0) / width;
+            var ratioY = image.GetLength(1) / height;
+            double stepX = 0;
+            double stepY = 0;
+            for (int x = 0; x < (int)width; x++)
+            {
+                for (int y = 0; y < (int)height; y++)
+                {
+                    resizedImage[x, y] = image[(int)stepX, (int)stepY];
+                    stepY += ratioY;
+                }
+                stepX += ratioX;
+                stepY = 0;
+            }
+
+            return resizedImage;
+        }
+    }
+}
---
+++ ImageToASCIIConverter/ImageToASCIIConverter/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace ImageToASCIIConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private string _textFile;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertImageBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog
+            {
+                Filter =
+                    "JPG Files (*.jpg)|*.jpg|bmp files (*.bmp)|*.bmp|JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)" +
+                    "|*.png|GIF Files (*.gif)|*.gif|All files (*.*)|*.*",
+                RestoreDirectory = true
+            };
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                var image = new BitmapImage(new Uri(fileDialog.FileName));
+                var converter = new Converters(image);
+                _textFile = converter.GetTextFile();
+                SourceImage.Source = image;
+
+                LogTextBox.Text += "Text file successfully created" + "\n";
+            }
+            else
+            {
+                LogTextBox.Text += "Error opening image" + "\n";
+            }
+        }
+
+        private void SaveTextFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_textFile != null)
+            {
+                var fileDialog = new SaveFileDialog
+                {
+                    FileName = "TextImageASCIIFile",
+                    DefaultExt = ".txt",
+                    Filter = "Text Files |.txt",
+                    RestoreDirectory = true
+                };
+
+                if (fileDialog.ShowDialog() == true)
+                {
+                    File.WriteAllText(fileDialog.FileName, _textFile, Encoding.ASCII);
+                    LogTextBox.Text += "Text file successfully saved" + "\n";
+                }
+                else
+                {
+                    LogTextBox.Text += "Error saving text file" + "\n";
+                }
+            }
+            else
+            {
+                LogTextBox.Text += "Text file is empty" + "\n";
+            }
+        }
+    }
+}
